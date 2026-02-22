5cd9ba397c60494b4b8a1f2a5639f1a4d2ad199d
sabikuntazina2025 <sabikuntazina@gmail.com>
2026-02-23 00:24:30 +0600

    added delete button and worked on its functionality

--- script.js
+++ script.js
@@ -12,3 +12,33 @@ function toggleStyle(id){
     selected.classList.remove('bg-white','text-black')
     selected.classList.add('bg-blue-500','text-white');
 }
+
+let totalJob = document.getElementById('total-job');
+let totalHeading = document.getElementById('total-heading');
+
+ document.getElementById('card-parents').addEventListener('click' , function(event){
+    
+    //  if(event.target.classList.contains('delete-btn')){
+    //     const card = event.target.parentNode;
+    //     card.remove();
+    // }
+    // else if(event.target.classList.contains('fa-trash-can')){
+    //     const card = event.target.parentNode.parentNode;
+    //     card.remove();
+
+    // }
+      const deleteBtn = event.target.closest('.delete-btn');
+
+    if(deleteBtn){
+        const card = deleteBtn.closest('.card');
+        card.remove();
+
+        let newTotalJob = Number(totalJob.innerText);
+        let newTotalHeading= Number(totalHeading.innerText);
+
+
+        totalJob.innerText = newTotalJob - 1;
+        totalHeading.innerText=newTotalHeading-1;
+
+    }
+})
